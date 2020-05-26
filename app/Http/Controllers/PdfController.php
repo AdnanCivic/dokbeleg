61215e69bc8dd82b1786fbb691354bb7f53a7320
Adnan Civic <ACivic@docubyte.local>
2020-05-26 11:43:34 +0200

    Testversion 2.0

--- app/Http/Controllers/PdfController.php
+++ app/Http/Controllers/PdfController.php
@@ -77,7 +77,11 @@ class PdfController extends Controller
         $name = 'Belegdokumentation';
         $html = '';
         foreach($bausteinTextArray as $baustein => $text){
-            $html .= $text;
+            if($baustein == 'deckblatt'){
+                $html .= $text;
+            }elseif($baustein == 'inhaltsverzeichnis'){
+                $html .= $text;
+            }
         }
 
         new PDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
@@ -85,15 +89,30 @@ class PdfController extends Controller
         PDF::SetCreator('PDF_Generator');
         PDF::SetAuthor('Docubyte');
         PDF::SetTitle('Verfahrensdokumentation zur Belegablage');
-        // PDF::SetPrintHeader(true);
-        // PDF::setHeaderData('', 0, 'marks', "Verfahrensdokumentation zur Belegablage");
-        // PDF::setHeaderFont(array('dejavusans', '', 14));
-        // PDF::SetHeaderMargin(30);
 
-        PDF::SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
+        PDF::setHeaderData('', 0, '', "Verfahrensdokumentation zur Belegablage");
+        PDF::setHeaderFont(array('dejavusans', '', 9));
+        PDF::SetHeaderMargin(5);
+
+        PDF::SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
+        PDF::SetAutoPageBreak(true, PDF_MARGIN_BOTTOM);
+        PDF::SetMargins(15, PDF_MARGIN_TOP, 15);
+
+        // PDF::setPrintFooter(true);
+        // PDF::setFooterData(array(0,64,0), array(0,64,128));
+        // PDF::setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
+
         PDF::AddPage('P', 'A4');
-        
         PDF::writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+
+        if(array_key_exists('vorbemerkungen', $bausteinTextArray)){
+            $html = $bausteinTextArray['vorbemerkungen'];
+        }
+        
+        PDF::AddPage('P', 'A4');
+        // PDF::writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
+        PDF::writeHTML($html, true, false, true, false, '');
+
         PDF::Output($name.'pdf', 'I');
 
     }
