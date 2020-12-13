5ba3dad8471f963f9647193d4349fbe1652ae283
Adnan Civic <adnan.civic@web.de>
2020-12-13 17:49:52 +0100

    router 1.0

--- resources/js/components/AlleBausteine.vue
+++ resources/js/components/AlleBausteine.vue
@@ -4,14 +4,15 @@
         <div class="card-body">
             <table class="table">
                 <tr><th>Name</th><th>ID</th><th>Nummer</th><th style="text-align: center">Aktion</th></tr>
-                <tr v-for="(baustein, index) in bausteine" :key="index"><td>{{ baustein.name }}</td><td>{{ baustein.id }}</td><td>{{ baustein.nummer }}</td>
-                    <td style="text-align:center"><a class="btn btn-primary">Anzeigen</a><a class="btn btn-danger">Löschen</a></td></tr>
+                <tr v-for="baustein in bausteine" :key="baustein.id"><td>{{ baustein.name }}</td><td>{{ baustein.id }}</td><td>{{ baustein.nummer }}</td>
+                    <td style="text-align:center"><a class="btn btn-primary" @click="showBaustein(baustein.id)">Anzeigen</a><a class="btn btn-danger">Löschen</a></td></tr>
             </table>
             <div class="text-center">
                 <div :class="{'spinner-border': isLoading}" style="width: 3rem; height: 3rem;"></div>
             </div>
         </div>
     </div>
+    <!-- <script src="/js/delete-confirm.js"></script> -->
 </template>
 
 <script>
@@ -42,6 +43,12 @@
                         this.isLoading = false;
                     });
             },
+
+            showBaustein: function(bausteinId){
+                console.log(bausteinId);
+                
+            },
+            
 
         },
 
--- resources/views/bausteine/index.blade.php
+++ resources/views/bausteine/index.blade.php
@@ -2,10 +2,9 @@
 
 @section('content')
 
-<div class="container" style="padding-top:70px;">
+<div class="container" style="margin-top:50px">
     <a class="btn btn-primary btn-large" href="{{ route('home') }}">Zurück</a>
     <alle-bausteine></alle-bausteine>
 </div>
-{{-- <script src="/js/delete-confirm.js"></script> --}}
 
 @endsection
